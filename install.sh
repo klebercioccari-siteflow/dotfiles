9306ae26e882738d64544a0b4313fa876e07b423
Kleber Cioccari <kleber.cioccari@siteflow.com>
2026-03-09 13:04:10 +0100

    feat: install zsh plugins and fzf

--- install.sh
+++ install.sh
@@ -17,6 +17,24 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# Extra plugins
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  echo "📦 Installing zsh-autosuggestions..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/fast-syntax-highlighting" ]; then
+  echo "📦 Installing fast-syntax-highlighting..."
+  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting "$ZSH_CUSTOM/plugins/fast-syntax-highlighting"
+fi
+
+if ! command -v fzf &>/dev/null; then
+  echo "📦 Installing fzf..."
+  apk add --no-cache fzf
+fi
+
 # Link dotfiles
 echo "🔗 Linking files..."
 ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
